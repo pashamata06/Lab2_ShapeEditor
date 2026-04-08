c713f0124557298437dd50790f5a1db399ba92d2
Bedunkevich Pavel <bedunkevich06@gmail.com>
2026-04-08 19:09:20 +0300

    Program.cs

--- ShapeEditor/obj/Debug/net10.0/ShapeEditor.AssemblyInfo.cs
+++ ShapeEditor/obj/Debug/net10.0/ShapeEditor.AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7559cd63d7b20554d094237bfbef42c9bc196f11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348d71e37fb93bf02eb4b841464d422083f35973")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
